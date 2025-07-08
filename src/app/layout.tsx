fc8c97ea1c2de1296a3eeb983e8b9eb04db2692f
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2025-07-08 09:07:18 +0600

    just start

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Nav/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Flex Fashion",
+  description:
+    "Flex Fashion - Your Fashion Destination. In this website, you can purchase t-shirts, hoodies, and more. Enjoy a seamless shopping experience with us.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar>{children}</Navbar>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Title from "@/components/Title";
+
+export default function Home() {
+  return (
+    <div className="w-full">
+      <Title>Flex Fashion</Title>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quaerat omnis
+      perferendis asperiores ducimus nisi quia officia quas reiciendis,
+      assumenda nostrum voluptatum in beatae similique nulla eaque quasi neque
+      recusandae velit dolor et sequi.
+    </div>
+  );
+}
